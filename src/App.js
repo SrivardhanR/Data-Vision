d03c10c6d91305db88bb8e78ec31d097a035d723
Srivardhan <srivardhanr9@gmail.com>
2025-02-10 16:53:49 +0530

    Resolved README.md conflict

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React ,{useEffect}from 'react';
+import {BrowserRouter,Routes, Route } from 'react-router-dom';
+import { FiSettings } from "react-icons/fi";
+import {TooltipComponent} from "@syncfusion/ej2-react-popups";
+
+import "./App.css";
+
+const App = () => {
+    const activeMenu = true;
+  return (
+    <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+            <div className="fixed right-4 bottom-4" style={{zIndex:'1000'}}>
+                <TooltipComponent content="Settings" position='top'>
+                    <button type="button" className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray' style={{background:'blue',borderRadius:'50%'}}>
+                        <FiSettings/>
+                    </button>
+                </TooltipComponent>
+            </div>
+            {activeMenu?(
+                <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+                    SideBar
+                </div>
+            ):(
+                <div className='w-0 dark:bg-secondary-dark-bg'></div>
+            )}
+        </div>
+    </BrowserRouter>
+  )
+}
+
+export default App;
